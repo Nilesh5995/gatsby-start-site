24fc8eb5ae96c4fd5ef6583a852343ce5af668f8
nilesh5995 <nilesh_s@techjoomla.com>
2019-11-21 12:31:20 +0530

    src added

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/new_header"
+export default () => (
+  <div style={{ color: `teal` }}>
+  <Header headerText="About Gatsby" />
+    <h1>About rise </h1>
+    <p>Such wow. Very React.</p>
+  </div>
+)
