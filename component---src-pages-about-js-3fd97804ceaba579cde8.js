9e48ada0fbde810502c89770542d5afd6523bda9
nilesh5995 <nilesh_s@techjoomla.com>
2019-11-21 13:57:27 +0530

    Updates

--- component---src-pages-about-js-3fd97804ceaba579cde8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(t,c,e){"use strict";e.r(c);e("q1tI");var n=e("qKvR");c.default=function(){return Object(n.c)("div",{style:{color:"teal"}},Object(n.c)("h1",null,"About Gatsby"),Object(n.c)("p",null,"Such wow. Very React."))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-3fd97804ceaba579cde8.js.map
--- component---src-pages-about-js-fa7dbcede4f83457a8c4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"3XHS":function(t,c,e){"use strict";e.r(c);e("q1tI");var n=e("qKvR");c.default=function(){return Object(n.c)("div",{style:{color:"teal"}},Object(n.c)("h1",null,"About Gatsby"),Object(n.c)("p",null,"Such wow. Very React."))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-fa7dbcede4f83457a8c4.js.map
